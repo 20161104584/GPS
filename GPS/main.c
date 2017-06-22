3a77a2e620ebdc5d447a5c5fa3b9831ca990dab9
20161104584 <a20161104584@mac26.local>
2017-06-22 10:10:07 +0800

    GPS111
    文件读取111

--- GPS/main.c
+++ GPS/main.c
@@ -15,7 +15,7 @@ int main (int argc, const char * argv[]) {
     char str2[70];
     fr=fopen("//Users//a20161104584//Desktop//GPS170510.log","r+");
     fscanf(fr,"%s%s",str1,str2);
-    printf("结果: %s\n%s\n",str1,str2);
+    printf("结 果: %s\n%s\n",str1,str2);
     fclose(fr);
     return 0;
 }
