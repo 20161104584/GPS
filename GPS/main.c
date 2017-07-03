e873963722bee9ad40131e974d3f9297980f9b44
20161104584 <a20161104584@mac26.local>
2017-07-03 08:48:48 +0800

    全部读取
    全部读取

--- GPS/main.c
+++ GPS/main.c
@@ -23,36 +23,52 @@ int main(int argc, const char * argv[]) {
     fw=fopen("//Users//a20161104584//Desktop//test//GPS//output.csv","w+");
     fscanf(fr,"%s%s",str1,str2);
     printf("结果: %s\n%s\n",str1,str2);
+    while(fscanf(fr,"%s%s",str1,str2)!=EOF)
+    {
     for(i=0;i<6;i++)
+    {
         time[i]=str1[i+7];
+    }
     time[6]='\0';
-    for(i=0;i<10;i++)
+    for(i=0;i<8;i++)
+    {
         lat[i]=str1[i+16];
-    lat[10]='\0';
-    for(i=0;i<11;i++)
+    }
+    lat[8]='\0';
+    for(i=0;i<9;i++)
+    {
         longitute[i]=str1[i+27];
-    longitute[11]='\0';
-    printf("经度: %s\n",longitute);
-    fprintf(fw,"经度: %s\n",longitute);
+    }
+    longitute[9]='\0';
+   
     for(i=0;i<6;i++)
+    {
         date[i]=str1[i+51];
+    }
     date[6]='\0';
     for(i=0;i<4;i++)
+    {
         high[i]=str2[i+43];
+    }
     high[4]='\0';
-    printf("海拔: %sm\n",high);
-    fprintf(fw,"海拔: %sm\n",high);
+  
     for(i=0;i<5;i++)
+    {
         speed[i]=str1[i+39];
+    }
     speed[5]='\0';
     for(i=0;i<5;i++)
+    {
         course[i]=str1[i+45];
+    }
     course[5]='\0';
     printf("时间,纬度,经度,日期,海拔,地面速率,地面航向\n");
     fprintf(fw,"时间,纬度,经度,日期,海拔,地面速率,地面航向\n");
     printf("%s,%s,%s,%s,%s,%s,%s\n",time,lat,longitute,date,high,speed,course);
     fprintf(fw,"%s,%s,%s,%s,%s,%s,%s\n",time,lat,longitute,date,high,speed,course);
+    }
     fclose(fr);
+    
     return 0;
     
 }
